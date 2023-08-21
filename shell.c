21273402f557d29a1f8432b6cbd1b7f049203fdb
JCRoooD <juancarlos-99@live.com>
2023-08-21 16:03:59 -0400

    added a way to not get double $

--- shell.c
+++ shell.c
@@ -11,37 +11,34 @@ int main(int ac, char **av, char **env)
 	char *buffer = NULL;
 	size_t bufsize = 0;
 	ssize_t characters_read;
+	int is_interactive = isatty(STDIN_FILENO);
 
 	while (1)
 	{
 		(void)ac;
 		(void)av;
 
-		printf("$ "); /*show prompt*/
+		if (is_interactive)
+		{
+			printf("$ "); /*show prompt*/
+		}
 		characters_read = getline(&buffer, &bufsize, stdin);
-        if (characters_read == -1)
-            break;
+		if (characters_read == -1)
+			break;
 
-       	buffer[strcspn(buffer, "\n")] = '\0';
+		buffer[strcspn(buffer, "\n")] = '\0';
 
-        if (strcmp(buffer, "exit") == 0)
-            break;
+		if (strcmp(buffer, "exit") == 0)
+			break;
 
-        if (isspace((unsigned char)buffer[0]))
-            continue;
+		if (isspace((unsigned char)buffer[0]))
+			continue;
 
-        if (buffer[0] == '\0' || buffer[0] == ' ')
-            continue;	
+		if (buffer[0] == '\0' || buffer[0] == ' ')
+			continue;
 
 		execute_command(buffer, env);
 	}
 	free(buffer);
 	return (0);
 }
-
-/**
-*
-*
-*
-*/
-
