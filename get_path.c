f9cf0608135393169a2b971a70cfe37842d3029a
JCRoooD <juancarlos-99@live.com>
2023-08-23 13:49:38 -0400

    added get path function to be able to get the path

---
+++ get_path.c
@@ -0,0 +1,34 @@
+#include "header_shell.h"
+/**
+ * extract_value - function to extract the value of an environment variable
+ * @entry: environment entry 
+ * Return: NULL
+ */
+char *extract_value(const char *entry)
+{
+	const char *equals = strchr(entry, '=');
+	if (equals)
+	{
+		return strdup(equals + 1);
+	}
+	return (NULL);
+}
+
+/**
+ * get_path - function that adds the path
+ * @env: environment variable
+ * Return: always NULL 
+ */
+char *get_path(char **env)
+{
+    char *path_variable;
+    for (int i = 0; env[i] != NULL; i++)
+    {
+        if (strncmp(env[i], "PATH=", 5) == 0)
+        {
+            path_variable = extract_value(env[i]);
+            break;
+        }
+    }
+	return (path_variable);
+}
