37d3da2a240a2933d57816a053eec5b17d400bb2
neisnei <130294790+neisnei@users.noreply.github.com>
2023-08-24 15:53:44 +0200

    Update exitprueba.c

--- exitprueba.c
+++ exitprueba.c
@@ -1,9 +1,20 @@
 #include "header_shell.h"
 
-void execute_command(char *command, char **env);
 
-printf("press enter to exit...\n");
-    getchar();
+int main() {
+    pid_t pid = fork();
+    if (pid == 0) {
+        // Child process
+        char* command = "comando_que_falla"; // Reemplaza esto con el comando que deseas ejecutar y que sabes que fallará
+        execlp(command, command, NULL);
+        exit(EXIT_FAILURE);
+    } else if (pid > 0) {
+        // Parent process
+        wait(NULL);
+    } else {
+        // Fork failed
+        printf("Fork failed\n");
+    }
 
     return 0;
 }
