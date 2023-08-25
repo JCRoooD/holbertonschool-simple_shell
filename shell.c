9428a6551082397720d88dae21c3368c64d8bf36
neisnei <130294790+neisnei@users.noreply.github.com>
2023-08-25 04:16:51 +0200

    Update shell.c

--- shell.c
+++ shell.c
@@ -32,19 +32,3 @@ int main(int ac, char **av, char **env)
 
 		if (strcmp(buffer, "exit") == 0)
 			break;
-
-		if (isspace((unsigned char)buffer[0]))
-			continue;
-
-		if (buffer[0] == '\0' || buffer[0] == ' ')
-			continue;
-		execute_command(buffer, env);
-	}
-	if (path_variable)
-	{
-		printf("the PATH value: %s\n", path_variable);
-		free(path_variable);
-	}
-	free(buffer);
-	return (0);
-}
