d08349f1b57228a5f948ba29502f3d1e7417b554
neisnei <130294790+neisnei@users.noreply.github.com>
2023-08-25 06:17:18 +0200

    Create exit.c

---
+++ exit.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // No se imprime nada en la salida estándar (stdout)
+    
+    // No se imprime nada en la salida de error estándar (stderr)
+    
+    // Retorna un valor de estado 0
+    return 0;
+}
