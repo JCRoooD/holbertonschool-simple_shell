d08e3d534cd878202f615ffe35c37f253e20db76
neisnei <130294790+neisnei@users.noreply.github.com>
2023-08-25 04:57:36 +0200

    Update execute_command.c

--- execute_command.c
+++ execute_command.c
@@ -1,38 +1,26 @@
 #include "header_shell.h"
-/**
- * execute_command - Ejecuta el comando ingresado por el usuario
- * @command: Comando ingresado por el usuario
- * @env: Variables de entorno
- */
 
-#include "header_shell.h"
+
 
 void execute_command(char *command, char **env)
 {
-pid_t pid;
-char *token, **tokens = NULL;
-int arg_count = 0;
+    char *token, **tokens = NULL;
+    int arg_count = 0;
 
-token = strtok(command, " \n");
-while (token != NULL)
-{
-    tokens = realloc(tokens, sizeof(char *) * (arg_count + 1));
-    tokens[arg_count++] = token;
-    token = strtok(NULL, " \n");
+    token = strtok(command, " \n");
+    while (token != NULL)
+    {
+        tokens = realloc(tokens, sizeof(char *) * (arg_count + 2));
+        tokens[arg_count++] = token;
+        token = strtok(NULL, " \n");
+    }
     tokens[arg_count] = NULL;
-}
 
-pid = fork();
-if (pid == -1)
-{
-    perror("fork");
-    return;
-}
-if (pid == 0)
-{
-    execve(tokens[0], tokens, env);
-    perror("Error ");
-    exit(EXIT_FAILURE);
+    if (fork() == 0)
+    {
+        execve(tokens[0], tokens, env);
+        perror("Error ");
+        exit(EXIT_FAILURE);
     }
     else
     {
