79b32f1c7360499962a878759a34293cfa954a5b
neisnei <130294790+neisnei@users.noreply.github.com>
2023-08-25 05:05:24 +0200

    Update execute_command.c

--- execute_command.c
+++ execute_command.c
@@ -1,24 +1,31 @@
 #include "header_shell.h"
-**/
-* execute_command - Execute the command entered by the user
-* @command: Command entered by the user
-* @env: Environment variables
-*/
+/**
+ * execute_command - Ejecuta el comando ingresado por el usuario
+ * @command: Comando ingresado por el usuario
+ * @env: Variables de entorno
+ */
 void execute_command(char *command, char **env)
 {
+    pid_t pid;
     char *token, **tokens = NULL;
     int arg_count = 0;
 
     token = strtok(command, " \n");
     while (token != NULL)
     {
-        tokens = realloc(tokens, sizeof(char *) * (arg_count + 2));
+        tokens = realloc(tokens, sizeof(char *) * (arg_count + 1));
         tokens[arg_count++] = token;
         token = strtok(NULL, " \n");
+        tokens[arg_count] = NULL;
     }
-    tokens[arg_count] = NULL;
 
-    if (fork() == 0)
+    pid = fork();
+    if (pid == -1)
+    {
+        perror("fork");
+        return;
+    }
+    if (pid == 0)
     {
         execve(tokens[0], tokens, env);
         perror("Error ");
