a9f86975982db62b05b98d41b8d456b7ce07fa83
neisnei <130294790+neisnei@users.noreply.github.com>
2023-08-25 05:57:18 +0200

    Create execute2.c

---
+++ execute2.c
@@ -0,0 +1,35 @@
+
+
+/**
+ * execute - execute command line
+ * @args: line of arguments
+ *
+ * Return: 0
+ */
+
+int execute(char **args)
+{
+	pid_t pid = fork();
+
+	if (pid == 0)
+	{
+		if (execvp(args[0], args) == -1)
+		{
+			perror("execvp error");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else if (pid > 0)
+	{
+		int status;
+
+		waitpid(pid, &status, 0);
+		return (WIFEXITED(status) ? WEXITSTATUS(status) : -1);
+	}
+	else
+	{
+		perror("fork error");
+		exit(EXIT_FAILURE);
+	}
+	return (0);
+}
