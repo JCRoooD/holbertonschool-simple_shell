5201ada7fad939e07b8b2b0277c41a6e09dd7cd4
neisnei <130294790+neisnei@users.noreply.github.com>
2023-08-25 05:12:51 +0200

    Update execute_command.c

--- execute_command.c
+++ execute_command.c
@@ -6,34 +6,45 @@
  */
 void execute_command(char *command, char **env)
 {
-    pid_t pid;
-    char *token, **tokens = NULL;
-    int arg_count = 0;
+	pid_t pid;
+	char **tokens = NULL;
+	int arg_count = 0;
 
-    token = strtok(command, " \n");
-    while (token != NULL)
-    {
-        tokens = realloc(tokens, sizeof(char *) * (arg_count + 1));
-        tokens[arg_count++] = token;
-        token = strtok(NULL, " \n");
-        tokens[arg_count] = NULL;
-    }
+	char *token = strtok(command, " \n");
 
-    pid = fork();
-    if (pid == -1)
-    {
-        perror("fork");
-        return;
-    }
-    if (pid == 0)
-    {
-        execve(tokens[0], tokens, env);
-        perror("Error ");
-        exit(EXIT_FAILURE);
-    }
-    else
-    {
-        wait(NULL);
-        free(tokens);
-    }
+	if (token == NULL)
+		return;
+	while (token != NULL)
+	{
+		tokens = realloc(tokens, sizeof(char *) * (arg_count + 1));
+		if (tokens == NULL)
+		{
+			perror("realloc");
+			return;
+		}
+		tokens[arg_count] = token;
+		arg_count++;
+		token = strtok(NULL, " \n");
+		tokens[arg_count] = NULL;
+	}
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		return;
+	}
+	if (pid == 0)
+	{
+
+		execve(tokens[0], tokens, env);
+		perror("Error ");
+		/* execve devuelve si ocurre un error */
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		/* Proceso padre */
+		wait(NULL); /* Esperar a que el proceso hijo termine */
+		free(tokens);
+	}
 }
